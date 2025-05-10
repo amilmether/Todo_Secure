8b0faaa3bb98410169a71918307c565498f875aa
Amil <amilmether37@gmail.com>
2025-05-11 00:39:47 +0530

    fix: Login issues

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-
+from app.database import engine
+from app import models
+from app.routes import user
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
+app.include_router(user.router)
+# app.include_router(todo.router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI Todo App"}
