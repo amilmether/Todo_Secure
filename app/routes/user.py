18b6460f8eb75b0e882cfcaf1166e22258871c9d
Amil <amilmether37@gmail.com>
2025-05-10 23:59:26 +0530

    Add user model, JWT auth, signup and login routes

---
+++ app/routes/user.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter,Depends,HTTPException,status
+from sqlalchemy.orm import Session
+from app import models,schemas,database,auth
+from fastapi.security import OAuth2PasswordRequestForm
+
+router = APIRouter(prefix="/auth",tags=["auth"])
+
+@router.post("/signup" , response_model=schemas.Token)
+def signup(user:schemas.UserCreate,db:Session = Depends(database.get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=400 , detail="Username already registered")
+    hashed_password = auth.get_password_hash(user.password)
+    new_user = models.User(
+        username=user.username,
+        email=user.email,
+        password=hashed_password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    access_token = auth.create_access_token(data={"sub":user.username})
+    return {"access_token":access_token,"token_type":"bearer"}
+@router.post("/login" , response_model=schemas.Token)
+def login(form_data:OAuth2PasswordRequestForm = Depends(),db:Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.username == form_data.username).first()
+    if not user or not auth.verify_password(form_data.password,user.password):
+        raise HTTPException(status_code=400,detail="Invalid credentials")
+    access_token = auth.create_access_token(data={"sub":user.username})
+    return {"access_token":access_token,"token_type":"bearer"}
