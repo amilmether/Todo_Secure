64ba4e1222f0a2129c4772cd18bce0c794255fa9
Amil <amilmether37@gmail.com>
2025-05-24 19:50:52 +0530

    renamed models1.py to models.py

--- app/db/models.py
+++ app/db/models.py
@@ -1,23 +1,26 @@
-from sqlalchemy import Column,Integer,String,Boolean,ForeignKey,DateTime
-from sqlalchemy.orm import relationship
+from sqlmodel import SQLModel,Field,Relationship
+from typing import Optional,List
 from datetime import datetime
-from app.db.database import Base
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer,primary_key=True, index=True)
-    username = Column(String,unique=True, index=True,nullable=False)
-    email = Column(String,nullable=False)
-    password = Column(String,nullable=False)
-    todos = relationship("Todo", back_populates="owner")
+class User(SQLModel,table=True):
+    __tablename__="Users"
+    id:Optional[int] = Field(default=None,primary_key=True,index=True)
+    username:str = Field(index=True,nullable=False,unique=True)
+    email:str=Field(nullable==False)
+    password:str=Field(nullable=False)
 
-class Todo(Base):
-    __tablename__ = "todos"
-    id = Column(Integer,primary_key=True,index=True)
-    title = Column(String,nullable=False)
-    description = Column(String)
-    done_by = Column(DateTime)
-    is_completed = Column(Boolean,default=False)
-    created_at = Column(DateTime,default=datetime.utcnow)
-    owner_id = Column(Integer,ForeignKey("users.id"))
-    owner = relationship("User",back_populates="todos")
+    todos:List["Todo"]=Relationship(back_populates="owner")
+
+class Todo(SQLModel,table=True):
+    __tablename__="todos"
+
+    id:Optional[int] = Field(default=None,primary_key=True,index=True)
+    title: str =Field(nullable=False)
+    description: Optional[str] = None
+    done_by: Optional[datetime] = None
+    is_completed: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    
+    owner_id: int = Field(foreign_key="users.id")
+    owner: Optional[User] = Relationship(back_populates="todos")
+
